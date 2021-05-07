794cf52057eb4232d3ee0a8da2cb8c7c42088c19
rmarticedeno <rmarticedeno@gmail.com>
2021-05-07 13:44:49 -0400

    query __str__ implementation

--- src/query.py
+++ src/query.py
@@ -4,3 +4,12 @@ class TestQuery:
     def __init__(self, text, relevant_documents):
         self.query = text
         self.relevant_documents = relevant_documents
+
+    def __str__(self):
+        return f'''
+        Query: {self.query}
+        Documents: {self.relevant_documents}
+        '''
+
+    def __repr__(self):
+        return self.__str__()
