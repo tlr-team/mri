0e5739506a3c7e44be6a35f18adff4895b6d3f19
Leonel Garcia <l.garcia3@estudiantes.matcom.uh.cu>
2021-05-13 16:05:39 -0400

    toJson mehtod for the Document model

--- src/document.py
+++ src/document.py
@@ -1,20 +1,35 @@
+from json import dumps
+
+
 class Document:
     def __init__(self, doc, text=True):
-        self.id = doc['id']
-        self.Title = doc.get('title', '')
+        self.id = doc.get('id', '')
+        self.title = doc.get('title', '')
         source = 'text' if text else 'abstract'
-        self.Desc = doc.get(source, '')
+        self.content = doc.get(source, '')
         self.author = doc.get('author', '')
+
+    def toJson(self):
+        dic = {}
+        dic['id'] = self.id
+        dic['title'] = self.title
+        dic['author'] = self.author
+        dic['content'] = self.content
+        return dumps(dic, indent=True)
 
     def __str__(self):
         return f'''
         Id: {self.id}
-        Title: {self.Title}
+        Title: {self.title}
         Author: {self.author}
 
-        Content: {self.Desc}
+        Content: {self.content}
         '''
 
     def __repr__(self):
-        return self.__str__()
+        return f'''
+        Id: {self.id}
+        Title: {self.title}
+        Author: {self.author}
+        '''
 
